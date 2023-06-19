8497cdcdf8c1e4e4efde96a9e9ce37641f764b62
MatteoGeusaDev <matteogeusadeveloper@gmail.com>
2023-06-19 21:58:44 +0200

    adding new pages

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-function menu() {
-   
 
-
-    var dropdownElement = document.querySelector(".dropdown-content ");
-    dropdownElement.style.animation = "move 1s";
-    dropdownElement.style.display = "flex"
-
-
-  }
-
-
-
-  function closemenu() {
-    var dropdownElement = document.querySelector(".dropdown-content");
-    dropdownElement.style.animation = "closemove 1s";
-  
-    setTimeout(function() {
-      dropdownElement.style.display = "none";
-    }, 990); // Ritardo di 2,5 secondi (2500 millisecondi)
-  }
 
   window.addEventListener("scroll", function() {
     var scrollArrow = document.querySelector(".scroll-arrow");
