5920df4b4f661d4129ad326c0632dc2a696199b5
MatteoGeusaDev <matteogeusadeveloper@gmail.com>
2023-06-20 17:06:05 +0200

    Sistemato il controllo con il menu e aggiunta delle informazioni dei dati personali

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 
 
-  window.addEventListener("scroll", function() {
-    var scrollArrow = document.querySelector(".scroll-arrow");
-    var scrollPosition = window.scrollY;
-  
-    if (scrollPosition > 0) {
-      
-      setTimeout(() => {
-        scrollArrow.style.display = "none";
-      }, 200); // Rimuovi l'animazione
-    } else {
-      scrollArrow.style.animation = "scrollAnimation 1.5s infinite"; // Ripristina l'animazione
-    }
-  });
+window.addEventListener("scroll", function() {
+  var scrollArrow = document.querySelector(".scroll-arrow");
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition > 0) {
+    
+    setTimeout(() => {
+      scrollArrow.style.display = "none";
+    }, 200); // Rimuovi l'animazione
+  } else {
+    scrollArrow.style.animation = "scrollAnimation 1.5s infinite"; // Ripristina l'animazione
+  }
+});
   
   
 
+function scrollButtonService() {
+  var section = document.getElementById("section2");
+  section.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollButtonPortfolio() {
+  var section = document.getElementById("section2");
+  section.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollButtonContact() {
+  var section = document.getElementById("section2");
+  section.scrollIntoView({ behavior: "smooth" });
+}
   
+
+
+function chiudiPannello() {
+  var pannello = document.getElementById('popup');
+  pannello.style.opacity = '0';
+  pannello.style.transition = 'opacity 0.5s';
+
+  setTimeout(function() {
+    pannello.style.display = 'none';
+  }, 500);
+}
